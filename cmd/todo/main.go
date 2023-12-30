509e951bd2ad5239c8e795b12b9ecc20a316e6da
Rajan <rajan.bangar83@gmail.com>
2023-12-30 19:07:38 +0530

    ability to add todo tasks added

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/rajan170/todo-cli"
+	// "fmt"
+)
+
+const (
+	todoFile = ".todos.json"
+)
 
 func main() {
-	fmt.Println("Hello GO!")
+	add := flag.Bool("add", false, "add a new todo")
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	switch {
+	case *add:
+		todos.Add("Sample Todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
 }
--- todo.go
+++ todo.go
@@ -1,7 +1,9 @@
 package todo
 
 import (
+	"encoding/json"
 	"errors"
+	"os"
 	"time"
 )
 
@@ -24,6 +26,13 @@ func (t *Todos) Add(task string) {
 	*t = append(*t, todo)
 }
 
+/*
+ls := *t: This line creates a local variable ls
+and assigns the dereferenced value of the receiver t
+to it. Similar to the previous example, it's done to
+make the code more concise and readable by using ls
+instead of *t throughout the method.
+*/
 func (t *Todos) Complete(index int) error {
 	ls := *t
 	if index <= 0 || index > len(ls) {
@@ -36,6 +45,13 @@ func (t *Todos) Complete(index int) error {
 	return nil
 }
 
+/*
+*t = append(ls[:index-1], ls[index:]...): If the index is valid,
+this line updates the todo list by removing the todo item at the
+specified index. It uses the append function to concatenate two
+slices: the elements before the specified index (ls[:index-1]) and
+the elements after the specified index (ls[index:]...).
+*/
 func (t *Todos) Delete(index int) error {
 	ls := *t
 	if index <= 0 || index > len(ls) {
@@ -44,4 +60,32 @@ func (t *Todos) Delete(index int) error {
 
 	*t = append(ls[:index-1], ls[index:]...)
 	return nil
+}
+
+func (t *Todos) Load(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+	if len(file) == 0 {
+		return err
+	}
+
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *Todos) Store(filename string) error {
+	data, err := json.Marshal(t)
+
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, data, 0644) //0644 is the unix-style permission code.
 }
